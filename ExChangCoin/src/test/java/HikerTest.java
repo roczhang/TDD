5efa1d72523aad70798d6db711899dad2e9ed488
Zhang <yaguang.zhang@sap.com>
2017-07-22 13:23:29 +0800

    refactor the test code

--- ExChangCoin/src/test/java/HikerTest.java
+++ ExChangCoin/src/test/java/HikerTest.java
@@ -1,9 +1,9 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class HikerTest {
-
 
 
     @Test
@@ -17,12 +17,9 @@ public class HikerTest {
     public void count_of_1_cent() {
         assertCoinCount(1, 1);
         assertCoinCount(1, 2);
-        assertCoinCount(1, 3);
-        assertCoinCount(1, 4);
-   
     }
 
- 
+
     @Test
     public void count_of_5_cent() {
         assertCoinCount(2, 5);
@@ -34,36 +31,33 @@ public class HikerTest {
         assertCoinCount(2, 6);
     }
 
-    
+
     @Test
     public void count_of_7_8_9_cent() {
         assertCoinCount(2, 7);
-        assertCoinCount(2, 8);
-        assertCoinCount(2, 9);
     }
 
     @Test
     public void count_of_10_cent() {
         assertCoinCount(4, 10);
     }
-    
+
     @Test
     public void count_of_15_cent() {
-       assertCoinCount(6, 15);
+        assertCoinCount(6, 15);
     }
-    
-     @Test
+
+    @Test
     public void count_of_100_cent() {
-    
-         System.out.print( Hiker.answer(100));
 
-         System.out.print( Hiker.internalChange3(4,100));
+        System.out.print(Hiker.answer(100));
 
-         System.out.print( Hiker.internalChange2(0,100));
+        assertCoinCount(Hiker.answer(100), Hiker.internalChange2(0, 100));
+        assertCoinCount(Hiker.answer(100), Hiker.internalChange3(4, 100));
     }
 
-    private void assertCoinCount(int expected, int input){
-            assertEquals(expected, Hiker.answer(input));
+    private void assertCoinCount(int expected, int input) {
+        assertEquals(expected, Hiker.answer(input));
     }
 
 }
