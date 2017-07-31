da8527aec0b11319ac1387958262d5ce1ee44c4b
Zhang <yaguang.zhang@sap.com>
2017-07-31 16:50:07 +0800

    refactor the code to Juint parameter runner

--- Balancedparentheses/src/test/java/HikerTest.java
+++ Balancedparentheses/src/test/java/HikerTest.java
@@ -1,46 +1,89 @@
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class HikerTest {
 
-    @Test
-    public void test_empty_parentheses() {
-       assertBalanced(true, "");
-       assertBalanced(true, "xxxxxx"); 
-    }
 
-    @Test
-    public void test_onlyOne_parentheses() {
-       assertBalanced(true, "()");
-       assertBalanced(false, ")(");
-    }
-
-    @Test
-    public void test_onlyOne_brackets() {
-       assertBalanced(true, "[]");
-       assertBalanced(false, "][");
-    }
-
-    @Test
-    public void test_onlyOne_brace() {
-       assertBalanced(true, "{}");
-       assertBalanced(false, "}{");
-    }
-
-    @Test
-    public void test_mix_brace() {
-       assertBalanced(false, "{{)(}}");
-       assertBalanced(false, "({)}");
-
-       assertBalanced(true, "[({})]");
-       assertBalanced(true, "{}([])");
-       assertBalanced(true, "{()}[[{}]]");       
-
-
+    @Parameterized.Parameters(name = "{index}:")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"", true},
+                {"xxxxxx", true},
+                {"()", true},
+                {")(", false},
+                {"[]", true},
+                {"][", false},
+                {"{}", true},
+                {"}{", false},
+                {"{{)(}}", false},
+                {"({)}", false},
+                {"[({})]", true},
+                {"{}([])", true},
+                {"{()}[[{}]]", true},
+        });
     }
 
 
-    private void assertBalanced( boolean expected, String input){    
+    private String input;
+    private boolean expected;
+
+    public HikerTest(String input, boolean expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(input + " : " + new Boolean(expected).toString());
         assertEquals(expected, Hiker.answer(input));
     }
+
+
+//    @Test
+//    public void test_empty_parentheses() {
+//       assertBalanced(true, "");
+//       assertBalanced(true, "xxxxxx");
+//    }
+//
+//    @Test
+//    public void test_onlyOne_parentheses() {
+//       assertBalanced(true, "()");
+//       assertBalanced(false, ")(");
+//    }
+//
+//    @Test
+//    public void test_onlyOne_brackets() {
+//       assertBalanced(true, "[]");
+//       assertBalanced(false, "][");
+//    }
+//
+//    @Test
+//    public void test_onlyOne_brace() {
+//       assertBalanced(true, "{}");
+//       assertBalanced(false, "}{");
+//    }
+//
+//    @Test
+//    public void test_mix_brace() {
+//       assertBalanced(false, "{{)(}}");
+//       assertBalanced(false, "({)}");
+//
+//       assertBalanced(true, "[({})]");
+//       assertBalanced(true, "{}([])");
+//       assertBalanced(true, "{()}[[{}]]");
+//
+//
+//    }
+//
+//
+//    private void assertBalanced( boolean expected, String input){
+//        assertEquals(expected, Hiker.answer(input));
+//    }
 }
